8fd9f9d994cbedfbfcce833359a6ef5cfd77cb04
gp888 <ping651803325@hotmail.com>
2019-03-04 20:30:12 +0800

    完善代码

--- lib/main0.dart
+++ lib/main0.dart
@@ -1,116 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
-void main() => runApp(new MyApp(
-//  items: new List<String>.generate(1000, (i)=>"Item $i")
-));
+void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget{
-
-//  final List<String> items;
-
-//  MyApp({Key key,@required this.items}):super(key:key);
-
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
-      title: 'gp.flutter.demo',
-      home: Scaffold(
-        appBar: new AppBar(
-          title: Text('ListView Widget'),
-        ),
-//        body: new ListView(
-//          children: <Widget>[
-////            new ListTile(
-////              leading: new Icon(Icons.android),
-////              title: new Text('android'),
-////            ),
-////            new ListTile(
-////              leading: new Icon(Icons.ac_unit),
-////              title: new Text('雪花是么的'),
-////            ),
-////            new ListTile(
-////              leading: new Icon(Icons.access_alarms),
-////              title: new Text('闹钟吧'),
-////            )
-//            new Image.network('https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png'),
-//            new Image.network('https://www.baidu.com/img/bd_logo1.png'),
-//            new Image.network('https://www.baidu.com/img/bd_logo1.png'),
-//            new Image.network('https://www.baidu.com/img/bd_logo1.png')
-//          ],
-//        )
-//        body: Center(
+  Widget build(BuildContext context) {
+    final wordPair = new WordPair.random();
+    return new MaterialApp(
+      title: 'Welcome to Flutter gp',
+      home: new Scaffold(
+        body: new Center(
+//            child: new Text(
+//              'Hello World 并且愿意为此奋斗一生我希望有水喝吧因为谁很好喝1999999999999999999999',
+//              textAlign: TextAlign.start,
+//              maxLines: 1,
+//              overflow: TextOverflow.ellipsis,
+//              style: TextStyle(
+//                  fontSize: 25.0,
+//                  color: Color.fromARGB(255, 255, 125, 125),
+//                  decoration: TextDecoration.underline,
+//                  decorationStyle: TextDecorationStyle.solid,
+//              ),
+//            )
+
 //          child: Container(
-//            height: 200.0,
-//            child: new MyList(
-//
+//            child: Text(
+//                'Hello gp',
+//                style: TextStyle(fontSize: 40.0)
+//            ),
+//            alignment: Alignment.topLeft,
+//            width:500.0,
+//            height:400.0,
+////            color: Colors.lightBlue,
+//            padding: const EdgeInsets.fromLTRB(10.0, 30.0, 0.0, 0.0),
+//            margin: const EdgeInsets.all(20.0),
+//            decoration: BoxDecoration(
+//              gradient: const LinearGradient(
+//                colors: [Colors.lightBlue, Colors.greenAccent, Colors.purple]
+//              ),
+//              border: Border.all(width: 2.0,color: Colors.red)
 //            ),
 //          ),
-//        ),
-//        body: ListView.builder(
-//          itemCount: items.length,
-//          itemBuilder: (context, index){
-//            return new ListTile(
-//              title: new Text('${items[index]}'),
-//            );
-//          },
-//        ),
-//        body: GridView.count(
-//          padding: const EdgeInsets.all(20.0),
-//          crossAxisSpacing: 10.0,
-//          crossAxisCount: 3,
-//          children: <Widget>[
-//            const Text('I am ap'),
-//            const Text('I am ap11'),
-//            const Text('I am ap22'),
-//            const Text('I am ap33')
-//          ],
-//        ),
-        body: GridView(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 4,
-              mainAxisSpacing: 2.0,
-              crossAxisSpacing: 2.0,
-              childAspectRatio: 0.75
+          child: Container(
+            child: Image.network(
+              'https://i0.hdslb.com/bfs/sycp/creative_img/201902/6eb8b6ea09db4b742dbe39ca0ca00b68.jpg',
+//              fit: BoxFit.scaleDown,//fill,contain,cover,fitWidth,fitHeight,scaleDown
+//              color: Colors.blueAccent,
+//              colorBlendMode: BlendMode.modulate,//darken,
+              repeat: ImageRepeat.repeat,
+
+            ),
+            width: 300.0,
+            height: 200.0,
+            color: Colors.green,
           ),
-          children: <Widget>[
-            new Image.network('http://img5.mtime.cn/mg/2019/02/15/150322.67691817.jpg', fit:BoxFit.cover),
-            new Image.network('http://img5.mtime.cn/mg/2019/01/24/141147.52006013.jpg', fit:BoxFit.cover),
-            new Image.network('http://img5.mtime.cn/mg/2019/02/15/145342.74515787.jpg', fit:BoxFit.cover),
 
-            new Image.network('http://img5.mtime.cn/mg/2019/02/15/101053.76458050_280X138X4.jpg', fit:BoxFit.cover),
-            new Image.network('http://img5.mtime.cn/mg/2019/02/15/102500.46139621_280X138X4.jpg', fit:BoxFit.cover),
-            new Image.network('http://img5.mtime.cn/mg/2019/02/12/011557.22476505_236X116X4.jpg', fit:BoxFit.cover),
-
-          ],
         ),
       ),
-    );
-  }
-}
-
-class MyList extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return ListView(
-      scrollDirection: Axis.horizontal,
-      children: <Widget>[
-        new Container(
-          width: 180.0,
-          color: Colors.lightBlue,
-        ),
-        new Container(
-          width: 180.0,
-          color: Colors.black,
-        ),
-        new Container(
-          width: 180.0,
-          color: Colors.amber,
-        ),
-        new Container(
-          width: 180.0,
-          color: Colors.purple,
-        )
-      ],
     );
   }
 }
---
+++ lib/main3.dart
@@ -0,0 +1,217 @@
+import 'package:flutter/material.dart';
+
+//void main(){
+//  runApp(MaterialApp(
+//    title: "导航演示1",
+//    home: FirstScreen(),
+//  ));
+//}
+//
+//class FirstScreen extends StatelessWidget{
+//  @override
+//  Widget build(BuildContext context){
+//    return Scaffold(
+//      appBar: AppBar(title: Text('导航页面')),
+//      body: Center(
+//        child: RaisedButton(
+//          child: Text('查看商品详情页'),
+//          onPressed: (){
+//            Navigator.push(context, MaterialPageRoute(
+//              builder: (context)=> SecondScreen()
+//            ));
+//          },
+//        ),
+//      ),
+//    );
+//  }
+//}
+//
+//class SecondScreen extends StatelessWidget{
+//  @override
+//  Widget build(BuildContext context){
+//    return Scaffold(
+//      appBar: AppBar(title: Text('gp 商品详情页')),
+//      body: Center(
+//        child: RaisedButton(
+//          child: Text('返回'),
+//          onPressed: (){
+//            Navigator.pop(context);
+//          },
+//        ),
+//      ),
+//    );
+//  }
+//}
+
+
+
+
+
+
+
+
+//class Product{
+////  final String title;
+////  final String description;
+////  Product(this.title,this.description);
+////}
+////
+////void main(){
+////  runApp(MaterialApp(
+////    title: '导航的数据传递和接受',
+////    home: ProductList(
+////      products:List.generate(
+////          20,
+////          (i)=>Product('商品 $i','商品详情编号$i'))
+////    ),
+////  ));
+////}
+////
+////class ProductList extends StatelessWidget{
+////
+////  final List<Product> products;
+////
+////  ProductList({Key key, @required this.products}):super(key:key);
+////
+////  @override
+////  Widget build(BuildContext context){
+////    return Scaffold(
+////      appBar: AppBar(title: Text('商品列表')),
+////      body: ListView.builder(
+////        itemCount: products.length,
+////        itemBuilder: (context,index){
+////          return ListTile(
+////            title: Text(products[index].title),
+////            onTap: (){
+////              Navigator.push(context, MaterialPageRoute(
+////                  builder:(context)=>ProductDetail(product:products[index])
+////              ));
+////            },
+////          );
+////        },
+////      ),
+////    );
+////  }
+////}
+////
+////
+////class ProductDetail extends StatelessWidget{
+////
+////  final Product product;
+////
+////  ProductDetail({Key key, @required this.product}):super(key:key);
+////
+////  @override
+////  Widget build(BuildContext context) {
+////    return Scaffold(
+////      appBar: AppBar(
+////        title: Text('${product.title}'),
+////      ),
+////      body: Center(child: Text('${product.description}')),
+////    );
+////  }
+////}
+
+
+
+
+
+
+
+
+
+
+//void main(){
+//  runApp(MaterialApp(
+//    title: '页面跳转返回数据',
+//    home: FirstPage(),
+//  ));
+//}
+//
+//class FirstPage extends StatelessWidget{
+//  @override
+//  Widget build(BuildContext context) {
+//    // TODO: implement build
+//    return Scaffold(
+//      appBar: AppBar(title:Text('找小姐姐要电话')),
+//      body: Center(
+//        child: RouteButton(),
+//      ),
+//    );
+//  }
+//}
+//
+//class RouteButton extends StatelessWidget{
+//  @override
+//  Widget build(BuildContext context) {
+//    // TODO: implement build
+//    return RaisedButton(
+//      onPressed: (){_navigateToXiaoJieJie(context);},
+//      child: Text('去找小姐姐'),
+//    );
+//  }
+//
+//  _navigateToXiaoJieJie(BuildContext context) async{
+//    final result = await Navigator.push(
+//        context,
+//        MaterialPageRoute(builder: (context)=>XiaoJieJie()));
+//
+//    Scaffold.of(context).showSnackBar(SnackBar(content: Text('$result')));
+//  }
+//}
+//
+//class XiaoJieJie extends StatelessWidget{
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(title: Text('我是小姐姐')),
+//      body: Center(
+//        child: Column(
+//          children: <Widget>[
+//            RaisedButton(
+//              child: Text('大长腿小姐姐'),
+//              onPressed: (){
+//                Navigator.pop(context, '大长腿小姐姐110');
+//              },
+//            ),
+//            RaisedButton(
+//              child: Text('大胸小姐姐'),
+//              onPressed: (){
+//                Navigator.pop(context,'大胸小姐姐119');
+//              },
+//            )
+//          ],
+//        ),
+//      ),
+//    );
+//  }
+//}
+
+
+
+
+
+void main()=>runApp(MyApp());
+
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container(
+      child: Image.asset('images/hu.pngfl'),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
